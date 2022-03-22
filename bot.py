9d37a9166c41ff9c9184fc18266c64b96aea6164
Camille <xdghost.kamil@gmail.com>
2022-03-22 15:58:49 +0300

    enabled infinity polling

--- bot.py
+++ bot.py
@@ -16,23 +16,23 @@ bot.set_my_commands([
 
 start_module(bot, weather_forecast)
 
-# bot.infinity_polling()
+bot.infinity_polling()
 
-@server.route('/' + token, methods=['POST'])
-def get_message():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
-
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://telegram-bot-xdkomel.herokuapp.com/' + token)
-    return "!", 200
-
-
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+# @server.route('/' + token, methods=['POST'])
+# def get_message():
+#     json_string = request.get_data().decode('utf-8')
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
+#
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url='https://telegram-bot-xdkomel.herokuapp.com/' + token)
+#     return "!", 200
+#
+#
+# server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
