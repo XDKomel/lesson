947c0f9968f6a41087e98a7c5342193658ea0874
Camille <xdghost.kamil@gmail.com>
2022-03-24 18:55:05 +0300

    first commit

--- bot.py
+++ bot.py
@@ -2,37 +2,44 @@ import telebot
 from telebot import types
 from weather_forecast import WeatherForecast
 from start_module import start_module
-import os
-from flask import Flask, request
+import schedule
+from threading import Thread
 
 token = '5189850186:AAHPqb5LybLL9uRTHkZKOHsIqn4HPD2OHGk'
 bot = telebot.TeleBot(token)
-server = Flask(__name__)
 weather_forecast = WeatherForecast()
+chat_id = None
 
 bot.set_my_commands([
-    types.BotCommand('start', 'Начните работу с ботом')
+    types.BotCommand('start', 'Начните работу с ботом'),
+    types.BotCommand('schedule', 'Запланируйте отправление сообщения в определенное время'),
 ])
 
-start_module(bot, weather_forecast)
+
+@bot.message_handler(commands=['schedule'])
+def schedule_hello(message):
+    global chat_id
+    chat_id = message.chat.id
+
+
+def send_hello():
+    if chat_id is not None:
+        bot.send_message(chat_id, 'hello')
+
+
+def schedule_checker():
+    while True:
+        schedule.run_pending()
+        schedule.clear()
+
+
+job = schedule.every().day.at('18:52').do(send_hello)
+Thread(target=schedule_checker).start()
+
 
 bot.infinity_polling()
 
-# @server.route('/' + token, methods=['POST'])
-# def get_message():
-#     json_string = request.get_data().decode('utf-8')
-#     update = telebot.types.Update.de_json(json_string)
-#     bot.process_new_updates([update])
-#     return "!", 200
-#
-#
-# @server.route("/")
-# def webhook():
-#     bot.remove_webhook()
-#     bot.set_webhook(url='https://telegram-bot-xdkomel.herokuapp.com/' + token)
-#     return "!", 200
-#
-#
-# server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+
 
 
