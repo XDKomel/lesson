b76204600d699b3a7f277ff3c43e37e3f13fe521
Camille <xdghost.kamil@gmail.com>
2022-04-05 18:05:55 +0300

    added start_module

--- bot.py
+++ bot.py
@@ -2,8 +2,7 @@ import telebot
 from telebot import types
 from weather_forecast import WeatherForecast
 from start_module import start_module
-import schedule
-from threading import Thread
+
 
 token = '5189850186:AAHPqb5LybLL9uRTHkZKOHsIqn4HPD2OHGk'
 bot = telebot.TeleBot(token)
@@ -16,25 +15,7 @@ bot.set_my_commands([
 ])
 
 
-@bot.message_handler(commands=['schedule'])
-def schedule_hello(message):
-    global chat_id
-    chat_id = message.chat.id
-
-
-def send_hello():
-    if chat_id is not None:
-        bot.send_message(chat_id, 'hello')
-
-
-def schedule_checker():
-    while True:
-        schedule.run_pending()
-        schedule.clear()
-
-
-job = schedule.every().day.at('18:52').do(send_hello)
-Thread(target=schedule_checker).start()
+start_module(bot, weather_forecast)
 
 
 bot.infinity_polling()
